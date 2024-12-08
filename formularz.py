7d18ac2217ff74fbca448aa5a62a568ba8e8a726
klapssen <98546572+klapssen@users.noreply.github.com>
2024-12-08 16:13:36 +0100

    Update formularz.py
    Dodano kolejne pola

--- formularz.py
+++ formularz.py
@@ -4,6 +4,10 @@ def potwierdzenie() :
     print("Zapisane:")
     print(pole1.get())
     print(pole2.get())
+    print(imie.get())
+    print(nazwisko.get())
+    print(email.get))
+    print(adres.get))
     exit()
 
 #Wstawiony komentarz 
@@ -18,6 +22,28 @@ etykieta2 = Label(okno, text="Dworska")
 etykieta2.grid(column = 00, row = 1, sticky="n")
 pole2 = Entry(okno)
 pole2.grid(column = 1, row = 1, sticky="n")
+
+etykietaImie = Label(okno, text="Imie")
+etykietaImie .grid(column = 00, row = 2, sticky="n")
+imie = Entry(okno)
+imie.grid(column = 1, row = 3, sticky="n")
+
+etykietaNazwisko = Label(okno, text="Nazwisko")
+etykietaNazwisko .grid(column = 00, row = 4, sticky="n")
+nazwisko = Entry(okno)
+nazwisko.grid(column = 1, row = 5, sticky="n")
+
+etykietaEmail = Label(okno, text="Email")
+etykietaEmail .grid(column = 00, row = 6, sticky="n")
+email = Entry(okno)
+email.grid(column = 1, row = 7, sticky="n")
+
+etykietaAdres = Label(okno, text="Adres")
+etykietaAdres .grid(column = 00, row = 8, sticky="n")
+adres = Entry(okno)
+adres.grid(column = 1, row = 9, sticky="n")
+
+
 zapis = Button(okno, text="Zapisz", command = potwierdzenie)
 zapis.grid(sticky="s")
 okno.mainloop()
